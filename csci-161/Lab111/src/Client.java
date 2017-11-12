676cdea9caee9e838c698cd45f251648b1e152e9
Joey Cluett <joesunglassescool@gmail.com>
2017-11-11 23:35:36 -0600

    added libs to project 111

--- csci-161/Lab111/src/Client.java
+++ csci-161/Lab111/src/Client.java
@@ -3,5 +3,21 @@
  * @author joey
  */
 public class Client {
+    public static void main(String[] args) {
+        // reallly wishing for typedef stuff right here:
+        TreeMap<Integer, Integer> unbalancedSearchTree  = new TreeMap<>();
+        AVLTreeMap<Integer, Integer> balancedSearchTree = new AVLTreeMap<>();   
+    }
+    
+    public static void testUnbalancedTree(TreeMap<Integer, Integer> ubst) {
+        System.out.println("== Unbalanced Tree ==");
+        System.out.println("Before any insertions: ");
+        System.out.println("Tree map size:   " + ubst.size());
+        System.out.println("Tree map height: " + ubst.depth());
+    }
+    
+    public static void testBalancedTree(AVLTreeMap<Integer, Integer> bst) {
+        
+    }
     
 }
